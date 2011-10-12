23a6d247c9c1a6cd116d5d8b733a01cba158febd
Christopher Reding <ccreding@gmail.com>
2011-10-11 19:20:40 -0500

    Adding the initial plugin to the repository

---
+++ Walker_Texas_Ranger_Menu.php
@@ -0,0 +1,369 @@
+<?php
+
+/*
+Plugin Name: WalkerTexasRangerConditionalMenu
+Plugin URI: 
+Description: Adds a conditional field to the menu
+Version: 1.0
+Author: Christopher Reding
+Author URI: http://christopherreding.com
+*/
+
+class Walker_Texas_Ranger_Menu_Edit extends Walker_Nav_Menu  {
+	/**
+	 * @see Walker_Nav_Menu::start_lvl()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference.
+	 */
+	function start_lvl(&$output) {}
+
+	/**
+	 * @see Walker_Nav_Menu::end_lvl()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference.
+	 */
+	function end_lvl(&$output) {
+	}
+
+	/**
+	 * @see Walker::start_el()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $item Menu item data object.
+	 * @param int $depth Depth of menu item. Used for padding.
+	 * @param object $args
+	 */
+	function start_el(&$output, $item, $depth, $args) {
+		global $_wp_nav_menu_max_depth;
+		$_wp_nav_menu_max_depth = $depth > $_wp_nav_menu_max_depth ? $depth : $_wp_nav_menu_max_depth;
+
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		ob_start();
+		$item_id = esc_attr( $item->ID );
+		$removed_args = array(
+			'action',
+			'customlink-tab',
+			'edit-menu-item',
+			'menu-item',
+			'page-tab',
+			'_wpnonce',
+		);
+
+		$original_title = '';
+		if ( 'taxonomy' == $item->type ) {
+			$original_title = get_term_field( 'name', $item->object_id, $item->object, 'raw' );
+			if ( is_wp_error( $original_title ) )
+				$original_title = false;
+		} elseif ( 'post_type' == $item->type ) {
+			$original_object = get_post( $item->object_id );
+			$original_title = $original_object->post_title;
+		}
+
+		$classes = array(
+			'menu-item menu-item-depth-' . $depth,
+			'menu-item-' . esc_attr( $item->object ),
+			'menu-item-edit-' . ( ( isset( $_GET['edit-menu-item'] ) && $item_id == $_GET['edit-menu-item'] ) ? 'active' : 'inactive'),
+		);
+
+		$title = $item->title;
+
+		if ( ! empty( $item->_invalid ) ) {
+			$classes[] = 'menu-item-invalid';
+			/* translators: %s: title of menu item which is invalid */
+			$title = sprintf( __( '%s (Invalid)' ), $item->title );
+		} elseif ( isset( $item->post_status ) && 'draft' == $item->post_status ) {
+			$classes[] = 'pending';
+			/* translators: %s: title of menu item in draft status */
+			$title = sprintf( __('%s (Pending)'), $item->title );
+		}
+
+		$title = empty( $item->label ) ? $title : $item->label;
+
+		?>
+		<li id="menu-item-<?php echo $item_id; ?>" class="<?php echo implode(' ', $classes ); ?>">
+			<dl class="menu-item-bar">
+				<dt class="menu-item-handle">
+					<span class="item-title"><?php echo esc_html( $title ); ?></span>
+					<span class="item-controls">
+						<span class="item-type"><?php echo esc_html( $item->type_label ); ?></span>
+						<span class="item-order">
+							<a href="<?php
+								echo wp_nonce_url(
+									add_query_arg(
+										array(
+											'action' => 'move-up-menu-item',
+											'menu-item' => $item_id,
+										),
+										remove_query_arg($removed_args, admin_url( 'nav-menus.php' ) )
+									),
+									'move-menu_item'
+								);
+							?>" class="item-move-up"><abbr title="<?php esc_attr_e('Move up'); ?>">&#8593;</abbr></a>
+							|
+							<a href="<?php
+								echo wp_nonce_url(
+									add_query_arg(
+										array(
+											'action' => 'move-down-menu-item',
+											'menu-item' => $item_id,
+										),
+										remove_query_arg($removed_args, admin_url( 'nav-menus.php' ) )
+									),
+									'move-menu_item'
+								);
+							?>" class="item-move-down"><abbr title="<?php esc_attr_e('Move down'); ?>">&#8595;</abbr></a>
+						</span>
+						<a class="item-edit" id="edit-<?php echo $item_id; ?>" title="<?php _e('Edit Menu Item'); ?>" href="<?php
+							echo ( isset( $_GET['edit-menu-item'] ) && $item_id == $_GET['edit-menu-item'] ) ? admin_url( 'nav-menus.php' ) : add_query_arg( 'edit-menu-item', $item_id, remove_query_arg( $removed_args, admin_url( 'nav-menus.php#menu-item-settings-' . $item_id ) ) );
+						?>"><?php _e( 'Edit Menu Item' ); ?></a>
+					</span>
+				</dt>
+			</dl>
+
+			<div class="menu-item-settings" id="menu-item-settings-<?php echo $item_id; ?>">
+				<?php if( 'custom' == $item->type ) : ?>
+					<p class="field-url description description-wide">
+						<label for="edit-menu-item-url-<?php echo $item_id; ?>">
+							<?php _e( 'URL' ); ?><br />
+							<input type="text" id="edit-menu-item-url-<?php echo $item_id; ?>" class="widefat code edit-menu-item-url" name="menu-item-url[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->url ); ?>" />
+						</label>
+					</p>
+				<?php endif; ?>
+				<p class="description description-thin">
+					<label for="edit-menu-item-title-<?php echo $item_id; ?>">
+						<?php _e( 'Navigation Label' ); ?><br />
+						<input type="text" id="edit-menu-item-title-<?php echo $item_id; ?>" class="widefat edit-menu-item-title" name="menu-item-title[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->title ); ?>" />
+					</label>
+				</p>
+				<p class="description description-thin">
+					<label for="edit-menu-item-attr-title-<?php echo $item_id; ?>">
+						<?php _e( 'Title Attribute' ); ?><br />
+						<input type="text" id="edit-menu-item-attr-title-<?php echo $item_id; ?>" class="widefat edit-menu-item-attr-title" name="menu-item-attr-title[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->post_excerpt ); ?>" />
+					</label>
+				</p>
+				<p class="field-link-target description description-thin">
+					<label for="edit-menu-item-target-<?php echo $item_id; ?>">
+						<?php _e( 'Link Target' ); ?><br />
+						<select id="edit-menu-item-target-<?php echo $item_id; ?>" class="widefat edit-menu-item-target" name="menu-item-target[<?php echo $item_id; ?>]">
+							<option value="" <?php selected( $item->target, ''); ?>><?php _e('Same window or tab'); ?></option>
+							<option value="_blank" <?php selected( $item->target, '_blank'); ?>><?php _e('New window or tab'); ?></option>
+						</select>
+					</label>
+				</p>
+				<p class="field-css-classes description description-thin">
+					<label for="edit-menu-item-classes-<?php echo $item_id; ?>">
+						<?php _e( 'CSS Classes (optional)' ); ?><br />
+						<input type="text" id="edit-menu-item-classes-<?php echo $item_id; ?>" class="widefat code edit-menu-item-classes" name="menu-item-classes[<?php echo $item_id; ?>]" value="<?php echo esc_attr( implode(' ', $item->classes ) ); ?>" />
+					</label>
+				</p>
+				<p class="field-xfn description description-thin">
+					<label for="edit-menu-item-xfn-<?php echo $item_id; ?>">
+						<?php _e( 'Link Relationship (XFN)' ); ?><br />
+						<input type="text" id="edit-menu-item-xfn-<?php echo $item_id; ?>" class="widefat code edit-menu-item-xfn" name="menu-item-xfn[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->xfn ); ?>" />
+					</label>
+				</p>
+				<?php if(!isset($item->conditional)) $item->conditional = ""; ?>
+				<p class="field-conditional description description-thin">
+					<label for="edit-menu-item-conditional-<?php echo $item_id; ?>">
+						<?php _e( 'Conditional' ); ?><br />
+						<input type="text" id="edit-menu-item-conditional-<?php echo $item_id; ?>" class="widefat code edit-menu-item-conditional" name="menu-item-conditional[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->conditional ); ?>" />
+					</label>
+				</p>
+				<p class="field-description description description-wide">
+					<label for="edit-menu-item-description-<?php echo $item_id; ?>">
+						<?php _e( 'Description' ); ?><br />
+						<textarea id="edit-menu-item-description-<?php echo $item_id; ?>" class="widefat edit-menu-item-description" rows="3" cols="20" name="menu-item-description[<?php echo $item_id; ?>]"><?php echo esc_html( $item->description ); // textarea_escaped ?></textarea>
+						<span class="description"><?php _e('The description will be displayed in the menu if the current theme supports it.'); ?></span>
+					</label>
+				</p>
+
+				<div class="menu-item-actions description-wide submitbox">
+					<?php if( 'custom' != $item->type && $original_title !== false ) : ?>
+						<p class="link-to-original">
+							<?php printf( __('Original: %s'), '<a href="' . esc_attr( $item->url ) . '">' . esc_html( $original_title ) . '</a>' ); ?>
+						</p>
+					<?php endif; ?>
+					<a class="item-delete submitdelete deletion" id="delete-<?php echo $item_id; ?>" href="<?php
+					echo wp_nonce_url(
+						add_query_arg(
+							array(
+								'action' => 'delete-menu-item',
+								'menu-item' => $item_id,
+							),
+							remove_query_arg($removed_args, admin_url( 'nav-menus.php' ) )
+						),
+						'delete-menu_item_' . $item_id
+					); ?>"><?php _e('Remove'); ?></a> <span class="meta-sep"> | </span> <a class="item-cancel submitcancel" id="cancel-<?php echo $item_id; ?>" href="<?php	echo esc_url( add_query_arg( array('edit-menu-item' => $item_id, 'cancel' => time()), remove_query_arg( $removed_args, admin_url( 'nav-menus.php' ) ) ) );
+						?>#menu-item-settings-<?php echo $item_id; ?>"><?php _e('Cancel'); ?></a>
+				</div>
+
+				<input class="menu-item-data-db-id" type="hidden" name="menu-item-db-id[<?php echo $item_id; ?>]" value="<?php echo $item_id; ?>" />
+				<input class="menu-item-data-object-id" type="hidden" name="menu-item-object-id[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->object_id ); ?>" />
+				<input class="menu-item-data-object" type="hidden" name="menu-item-object[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->object ); ?>" />
+				<input class="menu-item-data-parent-id" type="hidden" name="menu-item-parent-id[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->menu_item_parent ); ?>" />
+				<input class="menu-item-data-position" type="hidden" name="menu-item-position[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->menu_order ); ?>" />
+				<input class="menu-item-data-type" type="hidden" name="menu-item-type[<?php echo $item_id; ?>]" value="<?php echo esc_attr( $item->type ); ?>" />
+			</div><!-- .menu-item-settings-->
+			<ul class="menu-item-transport"></ul>
+		<?php
+		$output .= ob_get_clean();
+	}
+}
+
+class Walker_Texas_Ranger_Menu extends Walker {
+	/**
+	 * @see Walker::$tree_type
+	 * @since 3.0.0
+	 * @var string
+	 */
+	var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
+
+	/**
+	 * @see Walker::$db_fields
+	 * @since 3.0.0
+	 * @todo Decouple this.
+	 * @var array
+	 */
+	var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
+	/**
+	 * @see Walker::start_lvl()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int $depth Depth of page. Used for padding.
+	 */
+	function start_lvl(&$output, $depth) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"sub-menu\">\n";
+	}
+
+	/**
+	 * @see Walker::end_lvl()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int $depth Depth of page. Used for padding.
+	 */
+	function end_lvl(&$output, $depth) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent</ul>\n";
+	}
+
+	/**
+	 * @see Walker::start_el()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $item Menu item data object.
+	 * @param int $depth Depth of menu item. Used for padding.
+	 * @param int $current_page Menu item ID.
+	 * @param object $args
+	 */
+	function start_el(&$output, $item, $depth, $args) {
+		global $wp_query;
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		
+		$class_names = $value = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+		if( in_array('current-menu-item', $item->classes ) )
+		{
+			$func = $item->conditional;
+			if(function_exists($func)){
+				call_user_func($func);
+			}else{
+			global ${$func};
+			${$func} = create_function('$args', '$$args = TRUE;' );
+				//call_user_func($$func, $func);
+			}
+				
+		}else{
+			global ${$func};
+			${$func} = create_function('$args', '$$args = FALSE;' );
+			//call_user_func($$func, $func);
+
+			
+		}
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
+	/**
+	 * @see Walker::end_el()
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $item Page data object. Not used.
+	 * @param int $depth Depth of page. Not Used.
+	 */
+	function end_el(&$output, $item, $depth) {
+		$output .= "</li>\n";
+	}
+}
+
+
+add_action('wp_update_nav_menu_item', 'update_conditional_menu_item', 10, 3 );
+	
+function update_conditional_menu_item($menu_id, $menu_item_db_id, $args )
+{		
+		$args['menu-item-conditional'] =  $_POST['menu-item-conditional'][$menu_item_db_id];
+
+		if( isset( $_POST['menu-item-conditional'] ) ){
+			update_post_meta($menu_item_db_id, '_menu_item_conditional' , sanitize_key($args['menu-item-conditional']) );
+		} 
+
+}
+
+add_filter( 'wp_setup_nav_menu_item', 'add_conditional_menu');
+function add_conditional_menu( $menu_item )
+{
+	if( isset( $menu_item->conditional ) ) return $menu_item;
+	
+	$menu_item->conditional = get_post_meta($menu_item->ID, '_menu_item_conditional', TRUE );
+	
+	return $menu_item;
+	
+}
+
+add_filter( 'wp_nav_menu_args', 'conditional_nav_menu_args', 10, 1 );
+
+function conditional_nav_menu_args(  $args )
+{
+	if(!is_admin()){
+		$args['walker'] = new Walker_Texas_Ranger_Menu();
+	}
+	return $args;
+}
+
+if(is_admin()){
+add_filter( 'wp_edit_nav_menu_walker', 'My_Walker_Nav_Menu_Edit' );
+	function My_Walker_Nav_Menu_Edit(){
+		return 'Walker_Texas_Ranger_Menu_Edit';
+	}
+}
+
+
